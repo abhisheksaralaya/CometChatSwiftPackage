286e647165f25c293df6a9b211e9816a7188bb10
abhisheksaralaya <abhishek.saralaya@inscripts.in>
2021-07-20 04:30:29 +0530

    Starscream added

--- Package.swift
+++ Package.swift
@@ -20,9 +20,12 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "CometChatPro",
-            url: "https://library.cometchat.io/ios/v2.0/xcode12/CometChatPro.xcframework.zip",
+            url: "https://library.cometchat.io/ios/v2.0/xcode12/CometChatPro.xcframework_1.zip",
             checksum: "65c23852fac8bc863bd837351b9deb1a2bc3f7c35fc256374a5afaf32047f97c"
             
-        )
+        ),
+        .binaryTarget(name: "Starscream",
+                      url: "https://library.cometchat.io/ios/v2.0/xcode12/Starscream.xcframework_1.zip",
+                      checksum: "b0e9a80270f0e03d824e168620f0bd4c4f2aebd9050aa5c3ff724ceb86b950ba")
     ]
 )
