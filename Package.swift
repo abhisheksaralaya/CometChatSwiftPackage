cdce55240a247bf35c235793cb599e228614f8c1
abhisheksaralaya <abhishek.saralaya@inscripts.in>
2021-08-24 19:15:46 +0530

    Changes WIP

--- Package.swift
+++ Package.swift
@@ -21,6 +21,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(name: "CometChat"),
         .binaryTarget(
             name: "CometChatPro",
             url: "https://library.cometchat.io/ios/v2.0/xcode12/CometChatPro.xcframework_1.zip",
