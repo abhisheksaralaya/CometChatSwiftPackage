46d265b87848e8d8e83d59674629e45dfb10ad9a
abhisheksaralaya <abhishek.saralaya@inscripts.in>
2021-08-25 02:38:16 +0530

    Change WIP

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "CometChatPro",
+            name: "CometChatPro", type: .dynamic,
             targets: ["CometChatPro"]),
     ],
     dependencies: [
@@ -26,12 +26,12 @@ let package = Package(
         .binaryTarget(name: "Starscream", path: "./Sources/Starscream.xcframework")
 //        .binaryTarget(
 //            name: "CometChatPro",
-//            url: "https://library.cometchat.io/ios/v2.0/xcode12/CometChatPro.xcframework_1.zip",
+//            url: "https://library.cometchat.io/ios/v2.0/xcode12/CometChatPro.xcframework_1.zip", type: .dynamic,
 //            checksum: "0169d1cebbb98f5170016d4034f5b85f656f3efc7779ea476f6caa103398304d"
 //
 //        ),
 //        .binaryTarget(name: "Starscream",
-//                      url: "https://library.cometchat.io/ios/v2.0/xcode12/Starscream.xcframework_2.zip",
+//                      url: "https://library.cometchat.io/ios/v2.0/xcode12/Starscream.xcframework_2.zip", type: .dynamic,
 //                      checksum: "b0e9a80270f0e03d824e168620f0bd4c4f2aebd9050aa5c3ff724ceb86b950ba")
     ]
 )
