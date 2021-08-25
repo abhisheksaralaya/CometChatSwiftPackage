e108fc86b04932d1c32c9bb8040e21b55cb76384
abhisheksaralaya <abhishek.saralaya@inscripts.in>
2021-08-25 16:50:15 +0530

    Changes in packages WIP

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "CometChatPro", type: .dynamic,
+            name: "CometChatPro", 
             targets: ["CometChatPro"]),
     ],
     dependencies: [
@@ -21,17 +21,16 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(name: "CometChat"),
-        .binaryTarget(name: "CometChatPro", path: "./Sources/CometChatPro.xcframework"),
+//        .binaryTarget(name: "CometChatPro", path: "./Sources/CometChatPro.xcframework"),
 //        .binaryTarget(name: "Starscream", path: "./Sources/Starscream.xcframework")
-//        .binaryTarget(
-//            name: "CometChatPro",
-//            url: "https://library.cometchat.io/ios/v2.0/xcode12/CometChatPro.xcframework_1.zip", type: .dynamic,
-//            checksum: "0169d1cebbb98f5170016d4034f5b85f656f3efc7779ea476f6caa103398304d"
-//
-//        ),
-//        .binaryTarget(name: "Starscream",
-//                      url: "https://library.cometchat.io/ios/v2.0/xcode12/Starscream.xcframework_2.zip", type: .dynamic,
-//                      checksum: "b0e9a80270f0e03d824e168620f0bd4c4f2aebd9050aa5c3ff724ceb86b950ba")
+        .binaryTarget(
+            name: "CometChatPro",
+            url: "https://library.cometchat.io/ios/v2.0/xcode12/CometChatPro.xcframework_3.zip",
+            checksum: "0169d1cebbb98f5170016d4034f5b85f656f3efc7779ea476f6caa103398304d"
+
+        ),
+        .binaryTarget(name: "Starscream",
+                      url: "https://library.cometchat.io/ios/v2.0/xcode12/Starscream.xcframework_2.zip",
+                      checksum: "b0e9a80270f0e03d824e168620f0bd4c4f2aebd9050aa5c3ff724ceb86b950ba")
     ]
 )
